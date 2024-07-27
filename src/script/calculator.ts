b20cb6db4524d6b615cc9d2be4fff25c6f6263f4
Gabriel Bortoleti Di Rezze <bortoletigabriel.drz@gmail.com>
2024-07-27 15:33:57 -0300

    add: calculator script

---
+++ src/script/calculator.ts
@@ -0,0 +1,27 @@
+
+function getValue(inputId:string): number | null{
+
+    const inputField = document.getElementById(inputId) as HTMLInputElement | null;
+    if(inputField){
+        const value = inputField.value;
+        return parseFloat(value);
+    }
+    return null;
+
+}
+
+function calculateBMI( ) {
+
+    const height = getValue('heightInput');
+    const weight = getValue('weightInput');
+
+    if(height && weight){
+        const BMIValue = (weight / height**2);
+        return BMIValue;
+    }
+    
+    return null;
+
+}
+
+export default calculateBMI; getValue;
