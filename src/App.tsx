407d2dd0594bde6725d33aa427570ee256aa8499
Gabriel Bortoleti Di Rezze <bortoletigabriel.drz@gmail.com>
2024-07-28 16:25:21 -0300

    add: footer to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 import CalculatorApp from './components/calculator'
 import DynamicNav from './components/dynamicNav'
 import Home from './components/home'
+import Footer from './components/footer';
 
 const App: React.FC = () => {
 
@@ -13,6 +14,7 @@ const App: React.FC = () => {
       <main>
         <CalculatorApp />
       </main>
+      <Footer />
     </>
   )
 }
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,14 +1,16 @@
 import { FaGithub } from "react-icons/fa"
 
 const Footer = () =>{
-    <footer>
-        <a href="https://github.com/DiRezze" target="_blank" title="Meu perfil no GitHub">
-            <div className="d-flex f-row">
-                <FaGithub />
-                <span>@DiRezze on GitHub</span>
-            </div>
-        </a>
-    </footer>
+    return(
+        <footer>
+            <a href="https://github.com/DiRezze" target="_blank" title="Meu perfil no GitHub">
+                <div className="d-flex f-row">
+                    <FaGithub />
+                    <span>@DiRezze on GitHub</span>
+                </div>
+            </a>
+        </footer>
+    );
 }
 
-export default Footer;
+export default Footer
