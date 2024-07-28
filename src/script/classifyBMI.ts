35aa2d380613b190e3237754a909d744b629bc83
Gabriel Bortoleti Di Rezze <bortoletigabriel.drz@gmail.com>
2024-07-28 15:17:33 -0300

    fix: function param now can be null

--- src/script/classifyBMI.ts
+++ src/script/classifyBMI.ts
@@ -1,20 +1,26 @@
-export function classifyBMI (BMI:number){
+export function classifyBMI (BMI:number|null){
 
-    switch (true){
+    if(BMI){
 
-        case (BMI<20):
-            return "Abaixo do Ideal";
-        case (BMI<25):
-            return "Ideal";
-        case (BMI<30):
-            return "Sobrepeso"
-        case (BMI<35):
-            return "Obesidade (Grau 1)"
-        case (BMI<40):
-            return "Obesidade (Grau 2)"
-        default:
-            return "Obesidade (Grau 3)"
+        switch (true){
+    
+            case (BMI<20):
+                return "Abaixo do Ideal";
+            case (BMI<25):
+                return "Ideal";
+            case (BMI<30):
+                return "Sobrepeso"
+            case (BMI<35):
+                return "Obesidade (Grau 1)"
+            case (BMI<40):
+                return "Obesidade (Grau 2)"
+            default:
+                return "Obesidade (Grau 3)"
+    
+        }
 
     }
+
+    return null;
 
 }
