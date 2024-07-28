0b85b8b780a677151c9cb70bbffb717feba0669c
Gabriel Bortoleti Di Rezze <bortoletigabriel.drz@gmail.com>
2024-07-28 16:38:01 -0300

    rename: home.tsx => header.tsx

---
+++ src/components/header.tsx
@@ -0,0 +1,15 @@
+import { FiInfo } from "react-icons/fi";
+
+const Header:React.FC = () => {
+    return(
+        <header id='home'>
+            <h1>Calculadora de IMC</h1>
+            <div className="d-flex f-row justify-content-center align-items-center">
+                <FiInfo />
+                <h2 className="home-question">O que é IMC ?</h2>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
