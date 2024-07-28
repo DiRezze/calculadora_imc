6f2b46cfc0f6a4b61c60ff95d2a19d0da38cdd00
Gabriel Bortoleti Di Rezze <bortoletigabriel.drz@gmail.com>
2024-07-28 17:29:20 -0300

    information section css

--- src/components/info.tsx
+++ src/components/info.tsx
@@ -2,20 +2,36 @@ import { scrollTo } from "../script/scrollTo";
 
 const Information = ( ) =>{
     return(
-        <section id='info'>
-            <h2>Informações</h2>
-            <div>
-                <h3>O que é IMC?</h3>
-                <p>
-                    Um texto muito original e explicativo que com certeza não vou copiar do ChatGPT.
-                </p>
-                <button 
-                onClick={( ) => scrollTo('calculator')} 
-                type="button" 
-                className="btn btn-primary btn-lg"
-            >
-                Descubra o seu
-            </button>
+        <section id='info' className="info">
+            <h2 className="info-main-title">Informações</h2>
+            <div className=" d-flex flex-row">
+                
+                <div className="d-flex flex-column justify-content-start align-itens-center info-card">
+                    <h3 className="info-title">O que é IMC?</h3>
+                    <p>
+                        O Índice de Massa Corporal (IMC) é uma ferramenta amplamente utilizada para avaliar a quantidade de tecido corporal em relação à altura de uma pessoa. O IMC ajuda a classificar o peso de um indivíduo dentro de diferentes faixas, permitindo uma avaliação geral do estado nutricional e da saúde.
+                    </p>
+                    <button 
+                    onClick={( ) => scrollTo('calculator')} 
+                    type="button" 
+                    className="btn btn-primary btn-lg"
+                >
+                    Descubra o seu
+                </button>
+                </div>
+                <div className="d-flex flex-column justify-content-start align-itens-center info-card">
+                    <h3 className="info-title">IMC não é tudo</h3>
+                    <p>
+                        Embora o IMC seja uma medida prática e fácil de calcular, ele não leva em consideração a composição corporal, como a quantidade de massa muscular versus gordura. Portanto, pode não refletir com precisão a saúde de todos os indivíduos, especialmente aqueles com composição corporal atípica. Inclusive praticantes de atividades física plenamente saudáveis.
+                    </p>
+                    <button 
+                    onClick={( ) => scrollTo('calculator')} 
+                    type="button" 
+                    className="btn btn-primary btn-lg"
+                >
+                    Descubra o seu
+                </button>
+                </div>
             </div>
         </section>
     )
