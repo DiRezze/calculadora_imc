b243e13075f05a3d2dd3afb71af98d084d57b723
Gabriel Bortoleti Di Rezze <bortoletigabriel.drz@gmail.com>
2024-07-28 15:18:37 -0300

    add: BMI classification

--- src/components/results.tsx
+++ src/components/results.tsx
@@ -1,14 +1,18 @@
 import { calculateBMI } from "../script/calculator"
+import { classifyBMI } from "../script/classifyBMI";
 import BmiChart from "./graph";
 
 const Results = ( ) => {
+
     const BMIValue = calculateBMI();
+    const classification = classifyBMI(BMIValue);
+    
     if (BMIValue){
         return(
             <div className="results">
                 <div>
                     <h2>Seu IMC é {BMIValue}</h2>
-                    <h3>Um índice que é considerado <span>CLASSIFICAÇÃO</span>. </h3>
+                    <h3>Um índice que é considerado <span>{classification}</span>. </h3>
                 </div>
                 <button>Saiba mais</button>
                 <BmiChart />
