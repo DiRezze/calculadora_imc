a0b8e43df4ba9a49c1852abd2f03ba2c56492c6c
Gabriel Bortoleti Di Rezze <bortoletigabriel.drz@gmail.com>
2024-07-30 10:05:13 -0300

    add: header subtitle scrollTo function

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,15 @@
 import { FiInfo } from "react-icons/fi";
+import { scrollTo } from "../script/scrollTo";
 
 const Header:React.FC = () => {
     return(
         <header id='home'>
             <h1 className="main-title">Calculadora de IMC</h1>
-            <div className="d-flex f-row justify-content-center align-items-center">
-                <FiInfo />
+            <div 
+                className="d-flex f-row justify-content-center align-items-center home-question"
+                onClick={( ) => scrollTo('info')}
+            >
+                <FiInfo className="me-2" />
                 <h2 className="home-question">O que é IMC ?</h2>
             </div>
         </header>
