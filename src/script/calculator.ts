aeb6fb8dab9527d709ff6f0f91179741cf58d10a
Gabriel Bortoleti Di Rezze <bortoletigabriel.drz@gmail.com>
2024-07-31 14:32:37 -0300

    calculator.ts syntax

--- src/script/calculator.ts
+++ src/script/calculator.ts
@@ -1,6 +1,7 @@
 function getValue(inputId:string): number | null{
 
     const inputField = document.getElementById(inputId) as HTMLInputElement | null;
+    
     if(inputField){
         const value = inputField.value;
         return parseFloat(value);
@@ -15,9 +16,12 @@ export function calculateBMI( ) {
     const weight = getValue('weightInput');
 
     if(height && weight){
+
         const BMIValue = (weight / height**2);
         const BMIRounded = parseFloat(BMIValue.toFixed(1));
+
         return BMIRounded;
+
     }
 
     return null;
